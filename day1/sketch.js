7aec248ddf78d269eb35dc773653a939fb73721a
Greg K <gkreisman_cep@qq.com>
2024-02-15 21:30:18 +0100

    reversed the image

--- day1/sketch.js
+++ day1/sketch.js
@@ -10,13 +10,13 @@ let state = 0; // hold the state of the shower
 let lasttouch = 0; // for debouncing touch
 let first = true; // boolean for first touch
 
-CanvasRenderingContext2D.willReadFrequently = true
+CanvasRenderingContext2D.willReadFrequently = true;
 function setup() {
-    cnv = createCanvas(600,800 );
-    let cx = floor((windowWidth - cnv.width) / 2);
-    let cy = floor((windowHeight - cnv.height) / 2);
-    cnv.position(cx, cy);
-    imageMode(CENTER);
+  cnv = createCanvas(600, 800);
+  let cx = floor((windowWidth - cnv.width) / 2);
+  let cy = floor((windowHeight - cnv.height) / 2);
+  cnv.position(cx, cy);
+  imageMode(CENTER);
   if (width < height) {
     vScale = floor(width / 200); // vScale tied to window width so it can work on phone and computer
     console.log("by width");
@@ -26,60 +26,53 @@ function setup() {
   }
   pixelDensity(1);
   myvideo = createCapture(VIDEO);
-  myvideo.size(50,50);
+  myvideo.size(50, 50);
   myvideo.hide();
   // video dom element , the source, will be smaller by vScale which is 40 by 30 to improve performance
   //frameRate(15);
   noSmooth();
 }
 
-
 function touchStarted() {
-    // for Ios
-    // calculate time since last touch
-    const currenttime = millis();
-    const timesincelasttouch = currenttime - lasttouch;
-  
-    if (timesincelasttouch > 500) {
-      if (first) {
-        first = false;
-        print("first time");
-    
-      }
-      state++;
-      state = state % 2;
-   
-      // update
-      lasttouch = currenttime;
+  // for Ios
+  // calculate time since last touch
+  const currenttime = millis();
+  const timesincelasttouch = currenttime - lasttouch;
+
+  if (timesincelasttouch > 500) {
+    if (first) {
+      first = false;
+      print("first time");
+    }
+    state++;
+    state = state % 2;
+
+    // update
+    lasttouch = currenttime;
+  }
+}
+
+function mousePressed() {
+  touchStarted();
+  // for firefox computer browsers
+}
+function draw() {
+  background(0, 10);
+
+  for (let i = 0; i < 3; i++) {
+    particles.push(new Particle(random(100, 150), 20));
+  }
+
+  for (let particle of particles) {
+    let gravity = createVector(0, 0.9);
+    particle.applyForce(gravity);
+    particle.update();
+    particle.show();
+  }
+
+  for (let i = particles.length - 1; i >= 0; i--) {
+    if (particles[i].finished()) {
+      particles.splice(i, 1);
     }
   }
-  
-  
-  function mousePressed(){
-    touchStarted();
-    // for firefox computer browsers
-  }
-  function draw() {
-     
-      background(0,10);
-    
-  
-    
-      for (let i = 0; i < 3; i++) {
-        particles.push(new Particle(random(100,150),20 ));
-      }
-  
-      for (let particle of particles) {
-        let gravity = createVector(0, 0.9);
-        particle.applyForce(gravity);
-        particle.update();
-        particle.show();
-      }
-  
-      for (let i = particles.length - 1; i >= 0; i--) {
-        if (particles[i].finished()) {
-          particles.splice(i, 1);
-        }
-      }
-    
-  }
+}
